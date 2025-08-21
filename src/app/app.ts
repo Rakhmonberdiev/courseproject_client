5067d4f6f749b3f91f6de2c313601997f14463f2
Jamoliddin <j.m.raxmonberdiyev@gmail.com>
2025-08-21 11:32:29 +0500

    feat: добавить универсальный компонент text-input
    - создан standalone-компонент `app-text-input`, реализующий ControlValueAccessor
    - поддержка label, placeholder, type и кастомных CSS-классов через input-параметры
    - интеграция с ReactiveForms через NgControl
    - пример использования в форме регистрации/логина

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './shared/layout/navbar/navbar';
+import { TextInput } from './shared/forms/text-input/text-input';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, TextInput, ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {}
+export class App implements OnInit {
+  private fb = inject(FormBuilder);
+  registerForm: FormGroup = new FormGroup({});
+
+  ngOnInit(): void {
+    this.initTestForm();
+  }
+  initTestForm() {
+    this.registerForm = this.fb.group({
+      userName: ['', Validators.required],
+      email: ['', [Validators.email, Validators.required]],
+      password: ['', Validators.required],
+    });
+  }
+}
---
+++ src/app/shared/forms/text-input/text-input.ts
@@ -0,0 +1,52 @@
+import { Component, input, Self } from '@angular/core';
+import {
+  ControlValueAccessor,
+  FormControl,
+  NgControl,
+  ReactiveFormsModule,
+} from '@angular/forms';
+@Component({
+  selector: 'app-text-input',
+  imports: [ReactiveFormsModule],
+  templateUrl: './text-input.html',
+  styleUrl: './text-input.css',
+})
+export class TextInput implements ControlValueAccessor {
+  label = input<string>('');
+  type = input<string>('text');
+  placeholder = input<string>('');
+  inputClass = input<string>('');
+
+  constructor(@Self() public ngControl: NgControl) {
+    this.ngControl.valueAccessor = this;
+  }
+  value: any = '';
+  disabled = false;
+  private onChange: (val: any) => void = () => {};
+  private onTouched: () => void = () => {};
+
+  writeValue(val: any): void {
+    this.value = val ?? '';
+  }
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  onInput(e: Event) {
+    const v = (e.target as HTMLInputElement).value;
+    this.value = v;
+    this.onChange(v);
+  }
+  onBlur() {
+    this.onTouched();
+  }
+  get control(): FormControl {
+    return this.ngControl.control as FormControl;
+  }
+}
